70968b96da16bf26f827e70e34d71e007592ba6f
9c5s <98406118+9c5s@users.noreply.github.com>
2026-03-21 10:10:29 +0900

    fix: windowsでブロードキャストアドレスを正しく計算する
    Windows環境ではinterfaceAddress()がIPアドレスを返していたため、
    OptInパケットがブロードキャストされなかった。
    OS分岐を廃止しIPとサブネットマスクからブロードキャストアドレスを
    自前計算するように変更。broadcast-address依存を削除。

--- src/utils.ts
+++ src/utils.ts
@@ -1,24 +1,25 @@
-import broadcastAddress from "broadcast-address";
-import { networkInterfaces, platform } from "os";
+import { networkInterfaces } from "os";
+
+// IPv4アドレスとサブネットマスクからブロードキャストアドレスを計算する
+function calculateBroadcastAddress(address: string, netmask: string): string {
+    const addrParts = address.split(".").map(Number);
+    const maskParts = netmask.split(".").map(Number);
+    return addrParts.map((a, i) => a | (~maskParts[i] & 0xff)).join(".");
+}
 
 export function interfaceAddress(ifname: string): string {
-    const os = platform();
-
-    if (os === "win32") {
-        const intf = networkInterfaces()[ifname];
-        if (!intf) {
-            throw new Error(`Interface ${ifname} does not exist`);
-        }
-
-        const address = intf.find((el) => el.family == "IPv4");
-        if (!address) {
-            throw new Error(`Interface ${ifname} does not have IPv4 address`);
-        }
-
-        return address.address;
-    } else {
-        return broadcastAddress(ifname);
+    const interfaces = networkInterfaces();
+    const intf = interfaces[ifname];
+    if (!intf) {
+        throw new Error(`Interface ${ifname} does not exist`);
     }
+
+    const address = intf.find((el) => el.family === "IPv4");
+    if (!address) {
+        throw new Error(`Interface ${ifname} does not have IPv4 address`);
+    }
+
+    return calculateBroadcastAddress(address.address, address.netmask);
 }
 
 export const assert = (condition: boolean, message?: string): void => {
